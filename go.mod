fb285031e9278a72f1887e12bedb7bd92d7835b8
Devin McGloin <devin@mcgloin.email>
2021-06-09 21:21:41 -0700

    Splitbee solver

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/devinmcgloin/spellbee-solver
+
+go 1.16
+
+require (
+	github.com/beevik/prefixtree v0.0.0-20190221160703-0e2fef796dd6 // indirect
+	github.com/dghubble/trie v0.0.0-20210609182954-9a58e577d803 // indirect
+)
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/beevik/prefixtree"
+	"os"
+	"sort"
+	"strings"
+)
+
+var allowedCharacters = []string{"E", "A", "G", "C", "N", "I", "F"}
+var requiredCharacter = "F"
+
+func main() {
+	wordList, _ := readLines("Collins Scrabble Words (2019).txt")
+
+	trie := prefixtree.New()
+
+	for _, word := range wordList {
+		trie.Add(word, true)
+	}
+
+	bag := make([]string, 0)
+
+	recur([]string{}, trie, &bag)
+
+	sort.Slice(bag, func(i, j int) bool {
+		return len(bag[i]) > len(bag[j])
+	})
+
+	fmt.Println(bag)
+
+	for _, word := range bag {
+		matched := true
+		for _, character := range allowedCharacters {
+			if !strings.Contains(word, character) {
+				matched = false
+				break
+			}
+		}
+		if matched {
+			fmt.Println(word)
+		}
+	}
+}
+
+func recur(letters []string, trie *prefixtree.Tree, bag *[]string) {
+	word := strings.Join(letters[:], "")
+	val, err := trie.Find(word)
+	if err == prefixtree.ErrPrefixNotFound {
+		return
+	}
+
+	if (val != nil || err != prefixtree.ErrPrefixAmbiguous) && strings.Contains(word, requiredCharacter) && len(word) > 3 {
+		*bag = append(*bag, word)
+	}
+
+	for _, character := range allowedCharacters {
+		recur(append(letters, character), trie, bag)
+	}
+}
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
